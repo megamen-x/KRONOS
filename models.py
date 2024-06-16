518825f701697d5336a40e4f1cbe97eb62f6cf77
AlexeyLunyakov <a.lunyakov@my.com>
2024-06-16 04:38:04 +0300

    minor

---
+++ models.py
@@ -0,0 +1,6 @@
+from huggingface_hub import snapshot_download
+
+if __name__ == "__main__":
+    snapshot_download(repo_id="BAAI/bge-reranker-v2-m3", cache_dir="./reranker")
+    snapshot_download(repo_id="BAAI/bge-m3", cache_dir="./embedder")
+    snapshot_download(repo_id="IlyaGusev/saiga_llama3_8b", revision="main_vllm", cache_dir="./llm")
